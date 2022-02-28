1f14d7bd9396b323e5897543d115c7be5adcba36
Ray Sorenson <stngry78@gmail.com>
2022-02-28 09:47:45 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// name = 'ray';
+// var name;
+// //let name;
+// console.log(name);
+
+// setName();
+// function setName() {
+//     var name = 'Covalence';
+//     console.log(name);
+// }
+
+// console.log('some text1');
+// let avg = findAvg(2, 2);
+// console.log('some text2', avg);
+// function findAvg(a, b) {
+//     console.log('some text3');
+//     var answer = (a + b) / 2;
+//     return answer;
+// }
+
+// let fruits = ['apple', 'tomato', 'banana'];
+// //let favFruit;
+// function printFruits() {
+//     let favFruit = fruits[2];
+//     console.log(fruits[0]);
+//     printFavFruit();
+// function printFavFruit() {
+//     console.log(favFruit);
+//     //let leastFave = fruits[1];
+// }
+// //console.log(leastFave);
+// }
+// printFruits();
+
+wYWL();
+//whateverYouWouldLike();
+function whateverYouWouldLike() {
+    console.log('Hello, Norman');
+}
+
+let wYWL = whateverYouWouldLike();
